0006c9dbd2b1d5034a4b0030c04671a7ae9aaa57
CarlosRojas <crojasw100@gmail.com>
2025-03-31 22:10:52 -0500

    Creating project

---
+++ nanny/src/main/java/com/app/nanny/NannyApplication.java
@@ -0,0 +1,13 @@
+package com.app.nanny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NannyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NannyApplication.class, args);
+	}
+
+}
---
+++ nanny/src/test/java/com/app/nanny/NannyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.nanny;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NannyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
